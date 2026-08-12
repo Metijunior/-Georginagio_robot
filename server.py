c3a5a161b3bad89d482265a3a9d3f0f7a697230b
Metijunior <pimahti@gmail.com>
2026-08-12 20:39:30 +0330

    Update server.py

--- server.py
+++ server.py
@@ -3,18 +3,55 @@ import threading
 
 
 class Handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
+
+        if self.path == "/health":
+            self.send_response(200)
+            self.send_header(
+                "Content-Type",
+                "text/plain; charset=utf-8"
+            )
+            self.end_headers()
+
+            self.wfile.write(
+                b"OK"
+            )
+
+            return
+
+
         self.send_response(200)
+        self.send_header(
+            "Content-Type",
+            "text/plain; charset=utf-8"
+        )
         self.end_headers()
-        self.wfile.write(b"Bot is running")
+
+        self.wfile.write(
+            b"Bot is running"
+        )
+
+
+    def log_message(self, format, *args):
+        return
 
 
 def run_server():
-    server = HTTPServer(("0.0.0.0", 10000), Handler)
+
+    server = HTTPServer(
+        ("0.0.0.0", 10000),
+        Handler
+    )
+
     server.serve_forever()
 
 
 def start_server():
-    thread = threading.Thread(target=run_server)
-    thread.daemon = True
+
+    thread = threading.Thread(
+        target=run_server,
+        daemon=True
+    )
+
     thread.start()
